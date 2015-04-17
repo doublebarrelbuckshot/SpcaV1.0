0241dedfacefb21ab63066d5c33519230e50c4f5
khjoen <pascal@graviton.(none)>
2015-04-17 10:44:03 -0400

    NULL exception fix.  Search... not completed

--- app/src/main/java/com/example/jedgar/spcav10/MainPageFragment.java
+++ app/src/main/java/com/example/jedgar/spcav10/MainPageFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,9 +23,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class MainPageFragment extends Fragment implements View.OnClickListener {
 
@@ -36,22 +34,7 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
     ImageView img;
     static private boolean toggle = false;
     int action = 0;
-
-
-
-    public interface OnSearchListener {
-        public void doSearch();
-    }
-
-
-    //implemented onClick on each button when added to the inflater/// this one keep it empty else
-    //you cant have multiple buttons selected at the same time
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.search_button){
-            ((MainActivity)MainPageFragment.this.getActivity()).doSearch();
-        }
-    }
+    static SearchCriteria sc;
 
     ImageButton catButton;
     ImageButton dogButton;
@@ -59,11 +42,28 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
     ImageButton otherButton;
     Button searchButton;
     TextView ageText;
+    CheckBox femaleCheckBox;
+    CheckBox maleCheckBox;
+    RangeSeekBar<Integer> seekBar;
 
+    FragmentManager fragmentManager;
+
+    public interface OnSearchListener {
+        public void doSearch(SearchCriteria sc);
+    }
+
+
+    //implemented onClick on each button when added to the inflater/// this one keep it empty else
+    //you cant have multiple buttons selected at the same time
+    @Override
+    public void onClick(View v) {
+        ((MainActivity)MainPageFragment.this.getActivity()).doSearch(sc);
+    }
 
     public static MainPageFragment newInstance(){
 
         MainPageFragment fragment = new MainPageFragment();
+        sc = new SearchCriteria();
         return fragment;
     }
 
@@ -80,9 +80,11 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
         catButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sc.searchCats();
                 if(!v.findViewById(R.id.cat_button).isSelected()){
                     v.findViewById(R.id.cat_button).setSelected(true);
-                    Toast.makeText(MainPageFragment.this.getActivity(), "cat selected!", Toast.LENGTH_SHORT).show();}
+                    Toast.makeText(MainPageFragment.this.getActivity(), "cat selected!", Toast.LENGTH_SHORT).show();
+                }
                 else{
                     v.findViewById(R.id.cat_button).setSelected(false);
                 }
@@ -93,9 +95,11 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
         dogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sc.searchDogs();
                 if(!v.findViewById(R.id.dog_button).isSelected()){
                     v.findViewById(R.id.dog_button).setSelected(true);
-                    Toast.makeText(MainPageFragment.this.getActivity(), "dog selected!", Toast.LENGTH_SHORT).show();}
+                    Toast.makeText(MainPageFragment.this.getActivity(), "dog selected!", Toast.LENGTH_SHORT).show();
+                }
                 else{
                     v.findViewById(R.id.dog_button).setSelected(false);
                 }
@@ -107,6 +111,7 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
         rabbitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sc.searchRabbits();
                 if(!v.findViewById(R.id.rabbit_button).isSelected()){
                     v.findViewById(R.id.rabbit_button).setSelected(true);
                     Toast.makeText(MainPageFragment.this.getActivity(), "rabbit selected!", Toast.LENGTH_SHORT).show();}
@@ -120,6 +125,7 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
         otherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sc.searchSmallFurry();
                 if(!v.findViewById(R.id.other_button).isSelected()){
                     v.findViewById(R.id.other_button).setSelected(true);
                     Toast.makeText(MainPageFragment.this.getActivity(), "others selected!", Toast.LENGTH_SHORT).show();}
@@ -132,10 +138,8 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
         searchButton = (Button)rootView.findViewById(R.id.search_button);
         searchButton.setOnClickListener(this);
 
-
-
         // create RangeSeekBar as Integer range between 20 and 75
-        RangeSeekBar<Integer> seekBar = new RangeSeekBar<Integer>(0, 20, MainPageFragment.this.getActivity());
+        seekBar = new RangeSeekBar<Integer>(0, 20, MainPageFragment.this.getActivity());
         seekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
             @Override
             public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
@@ -145,18 +149,47 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
                         getResources().getString(R.string.rsbAgeFrom) + minValue +
                                 getResources().getString(R.string.rsbAgeTo)+ maxValue +
                                         getResources().getString(R.string.rsbAgeYears));
+                sc.setAgeMin(minValue);
+                sc.setAgeMax(maxValue);
             }
         });
 //https://github.com/yahoo/android-range-seek-bar
         LinearLayout layout = (LinearLayout) rootView.findViewById(R.id.seekbar_placeholder);
         layout.addView(seekBar);
 
+        maleCheckBox = (CheckBox)rootView.findViewById(R.id.male_checkBox);
+        maleCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sc.searchMales();
+                if(!v.findViewById(R.id.male_checkBox).isSelected()){
+                    v.findViewById(R.id.male_checkBox).setSelected(true);
+                    Toast.makeText(MainPageFragment.this.getActivity(), "males selected!", Toast.LENGTH_SHORT).show();}
+                else{
+                    v.findViewById(R.id.male_checkBox).setSelected(false);
+                }
+            }
+        });
+
+        femaleCheckBox = (CheckBox)rootView.findViewById(R.id.female_checkBox);
+        femaleCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sc.searchFemales();
+                if(!v.findViewById(R.id.female_checkBox).isSelected()){
+                    v.findViewById(R.id.female_checkBox).setSelected(true);
+                    Toast.makeText(MainPageFragment.this.getActivity(), "females selected!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    v.findViewById(R.id.female_checkBox).setSelected(false);
+                }
+            }
+        });
 
         ageText.setText(getResources().getString(R.string.age_text_view) +
                 getResources().getString(R.string.rsbAgeFrom) + seekBar.getSelectedMinValue() +
                 getResources().getString(R.string.rsbAgeTo)+ seekBar.getSelectedMaxValue() +
                 getResources().getString(R.string.rsbAgeYears));
-
 
         progressBar = (ProgressBar)rootView.findViewById(R.id.webProgressBar);
         progressBar.setVisibility(View.GONE);
@@ -175,6 +208,7 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
     public void getData(){//View v) {
         Log.d("D", "in getData()");
         if (dbh.appFirstTime(db)) {
+        //{
             Toast.makeText(getActivity(), "Chargement des donnees du Web", Toast.LENGTH_SHORT).show();
             try {
                 new DownloadAdoptableSearch(progressBar, progressText, searchButton, getActivity()).execute();// REMOVE COMMENT FOR PROPER FUNCTIONING
@@ -184,10 +218,10 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
             }
             dbh.appFirstTimeFalse(db);
         }
-        /*
-        else {
+        /*else {
             //Toast.makeText(getActivity(), "Chargement des favoris", Toast.LENGTH_SHORT).show();
             Cursor animalList = dbh.getAnimalListOrdered(db, dbh.T_ANIMAL_ID + " asc ");
+            Log.d("DownloadAdoptableSearch", "animalList size = " + animalList.getCount());
             try {
                 Log.d("DownloadAdoptableSearch", "About to call trigger AdopdatableSearch in update mode.");
                 new DownloadAdoptableSearch(progressBar, progressText, searchButton, getActivity(), animalList).execute();// REMOVE COMMENT FOR PROPER FUNCTIONING
@@ -196,8 +230,10 @@ public class MainPageFragment extends Fragment implements View.OnClickListener {
                 return;
             }
 
+            animalList = dbh.getAnimalList(db);
             //adapter.changeCursor(animalList);
             //btn.setEnabled(true);
+
         }*/
     }
 
