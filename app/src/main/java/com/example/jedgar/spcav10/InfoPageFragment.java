8b29179e23763e167f2b9ec7d27681ebcf600670
Juan Edgar <donjuanedgar@gmail.com>
2015-04-21 01:44:11 -0400

    info_page_fragment  now has  4 tabs ( general -dogs - cats - others) color for the app are the red like we agreed

--- app/src/main/java/com/example/jedgar/spcav10/InfoPageFragment.java
+++ app/src/main/java/com/example/jedgar/spcav10/InfoPageFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class InfoPageFragment extends Fragment implements /*View.OnClickListener*/ GetActionBarTitle {
+
+    private FragmentTabHost tabHost;
+
+
+
 /*
     ImageButton buttonSend;
     TextView ident;
@@ -43,8 +49,33 @@ public class InfoPageFragment extends Fragment implements /*View.OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.info_page_fragment, container, false);
-        return rootView;
+        tabHost = new FragmentTabHost(getActivity());
+        tabHost.setup(getActivity(), getChildFragmentManager(), R.layout.info_page_fragment);
+
+        Bundle arg1 = new Bundle();
+        arg1.putInt("Arg for Frag1", 1);
+        tabHost.addTab(tabHost.newTabSpec("Tab1").setIndicator(getString(R.string.general_tab)),
+                GeneralInfoFragment.class, arg1 );
+
+        Bundle arg2 = new Bundle();
+        arg2.putInt("Arg for Frag2", 2);
+        tabHost.addTab(tabHost.newTabSpec("Tab2").setIndicator(getString(R.string.cat_tab)),
+                CatInfoFragment.class, arg2);
+
+        Bundle arg3 = new Bundle();
+        arg3.putInt("Arg for Frag3", 3);
+        tabHost.addTab(tabHost.newTabSpec("Tab3").setIndicator(getString(R.string.others_tab)),
+                OthersInfoFragment.class, arg3);
+
+        Bundle arg4 = new Bundle();
+        arg4.putInt("Arg for Frag4", 4);
+        tabHost.addTab(tabHost.newTabSpec("Tab4").setIndicator(getString(R.string.dog_tab)),
+                DogInfoFragment.class, arg4);
+
+        return tabHost;
+
+       // View rootView = inflater.inflate(R.layout.info_page_fragment, container, false);
+        //return rootView;
     }
 
 
