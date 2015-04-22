f06d945813da09a406b5075769f99050410d293e
young_000 <simo.boutara@gmail.com>
2015-04-22 00:49:38 -0400

    notification suite2

--- app/src/main/java/com/example/jedgar/spcav10/ActivityNotification.java
+++ app/src/main/java/com/example/jedgar/spcav10/ActivityNotification.java
@@ -8,6 +8,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,13 +27,13 @@ public class ActivityNotification extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.favorites_page_fragment);
+        setContentView(R.layout.browse_row);
 
 
-       /* SearchCriteria sc = new SearchCriteria(BrowsePageFragment.db);
+        SearchCriteria sc = new SearchCriteria(BrowsePageFragment.db);
         String sql = new String(sc.getCommandForNotifs());
         dbh.setCursorForSelect(BrowsePageFragment.db, sql, DBHelper.CURSOR_NAME_NEW_ANIMALS);
-        c = dbh.getCursorForSelect(DBHelper.CURSOR_NAME_NEW_ANIMALS);*/
+        c = dbh.getCursorForSelect(DBHelper.CURSOR_NAME_NEW_ANIMALS);
 
        // c.moveToFirst();
       //  Toast.makeText(this, "c.isAfterLast()" + c.isAfterLast(), Toast.LENGTH_SHORT).show();
@@ -46,7 +51,6 @@ public class ActivityNotification extends Activity {
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.cancel(AlarmReceiver.ID_NOTIFICATION);
     }
-
-
-
 }
+
+
